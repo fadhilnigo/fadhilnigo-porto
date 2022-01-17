59112b94629594eecbf5f4fa538175a146454ed9
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-18 02:17:00 +0700

    Add footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-white">
+      <p>© Copyright 2022. Designed by <a>@fitrakun</a> developed by <a>@fadhilnigo</a></p>
+    </footer>
+  )
+}
+
+export default Footer
