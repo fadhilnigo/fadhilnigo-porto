cd9532ac1fcbeadf0d3e719ff30b9ae947f73f84
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-17 23:37:56 +0700

    Add navigation bar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="navbar">
+        <a className="nav-link">PORTOFOLIO</a>
+        <a className="nav-link">CONTACT</a>  
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
