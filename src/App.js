8d8bb3282dde983ca4d07199d202c3dfc47c01c4
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-17 23:36:19 +0700

    Boilerplate for styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import Navbar from './component/Navbar'
+import AOS from "aos";
+import "aos/dist/aos.css"
 
 function App() {
+  useEffect(() => {
+    AOS.init()
+    AOS.refresh();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/project/:projectId'} element={<Project />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
