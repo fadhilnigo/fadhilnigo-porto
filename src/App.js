eaebd35755e843c17ab960b5ec38167659e901a1
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-18 02:16:41 +0700

    Add profile picture

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home";
 import Project from "./pages/Project";
 import Navbar from './component/Navbar'
+import Footer from "./component/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css"
 
@@ -20,6 +21,7 @@ function App() {
           <Route path={'/'} element={<Home />} />
           <Route path={'/project/:projectId'} element={<Project />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,41 @@
 import profile from '../assets/images/profile.png'
+import vectorBlue from '../assets/images/vector-blue.svg'
+import vectorPink from '../assets/images/vector-pink.svg'
+import starPink from '../assets/images/star-pink.svg'
+import starBlue from '../assets/images/star-blue.svg'
 
 function Home() {
   return (
     <>      
-      <header className="header-home">
-        <section className="section-container">
-          <div className="section-content">
+      <header className="home-container">
+        <section className="section-container bg-white">
+          <div className="section-content" data-aos="zoom-in">
             <h1>Hi, I'm Fadhil Panigoro Rahardjo, a <span>self-taught</span> front-end developer.</h1>
             <p>Look around and see my portfolio</p>
             <button className="btn--pils">SEE MY PORTFOLIO</button>
           </div>
         </section>
         <section className="section-container bg-blue">
-          <div className="img-container">
-            <img src={profile} alt="fadhil" className="header-img" />
+          <div className="img-container" data-aos="zoom-in">
+            <img src={profile} alt="fadhil" className="profile-img" />
+            <img src={vectorBlue} alt="background" className="background-img shape-blue" />
+            <img src={vectorPink} alt="background" className="background-img shape-pink" />
+            <img src={starBlue} alt="background" className="background-img star-blue" />
+            <img src={starPink} alt="background" className="background-img star-pink" />
           </div>
         </section>
       </header>
+
+      {/* Swiper */}
+
+      <body className="home-container">
+        <section className="section-container bg-white">
+          <h1>This is section one</h1>
+        </section>
+        <section className="section-container  bg-blue">
+          <h1>This is section two</h1>
+        </section>
+      </body>
     </>
   )
 }
