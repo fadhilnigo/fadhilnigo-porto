c886dd82cdbbd7f34bbc8932608bb43776d0e715
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-19 20:33:01 +0700

    Add about me and contact in section in home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import vectorBlue from '../assets/images/vector-blue.svg'
 import vectorPink from '../assets/images/vector-pink.svg'
 import starPink from '../assets/images/star-pink.svg'
 import starBlue from '../assets/images/star-blue.svg'
+import mailIcon from '../assets/images/mail.svg'
+import githubIcon from '../assets/images/github.svg'
 
 function Home() {
   return (
@@ -28,12 +30,47 @@ function Home() {
 
       {/* Swiper */}
 
-      <body className="home-container">
-        <section className="section-container bg-white">
-          <h1>This is section one</h1>
+      <body className="body-container">
+        <section className="section-container section-left bg-white">
+          <div>
+            <h1 data-aos="zoom-in">About Me</h1>
+            <p data-aos="zoom-in">Hi! My name is Fadhil Panigoro Rahardjo and I started to challenge myself to 
+              follow the latest technology by learning and programming using Next.js.
+              <br />
+              I'm a self-taught front-end developer who will constantly seeking out new technologies 
+              and staying up-to-date on trends in the industry even though my major is Bioenergy and kemurgy engineering.   
+            </p>
+            <p data-aos="zoom-in">Here are few technologies I've been learning on recently:</p>
+            <div>
+              <p className="body-list" data-aos="zoom-in">
+                <img src={vectorPink} alt="List Style" className="list-img" />
+                React.js
+              </p>
+              <p className="body-list" data-aos="zoom-in">
+                <img src={vectorPink} alt="List Style" className="list-img" />
+                Next.js
+              </p>
+              <p className="body-list" data-aos="zoom-in">
+                <img src={vectorPink} alt="List Style" className="list-img" />
+                Firebase
+              </p>
+            </div>
+          </div>
         </section>
-        <section className="section-container  bg-blue">
-          <h1>This is section two</h1>
+        <section className="section-container section-right bg-blue">
+          <div className="content-container">
+            <h1 data-aos="zoom-in">Get in Touch</h1>
+            <p data-aos="zoom-in">Feel free to contact me anytime! I'll get back to you as soon as possible. </p>
+            <p className="text-contact" data-aos="zoom-in">
+              <img src={mailIcon} alt="Mail Icon" className="body-icon" />
+              fadhilnigo@gmail.com
+            </p>
+            <p data-aos="zoom-in">Know more about my code by looking around my github!</p>
+            <a href="https://github.com/fadhilnigo" className="text-contact" data-aos="zoom-in">
+              <img src={githubIcon} alt="Github Icon" className="body-icon" />
+              https://github.com/fadhilnigo
+            </a>
+          </div>
         </section>
       </body>
     </>
