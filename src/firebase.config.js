2f5e8c3ead9daf11792290d365474a28f72b92dc
LAPTOP-P69CJES2\Fadhil <fadhilnigo@gmail.com>
2022-01-28 23:26:05 +0700

    Private API

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@ import { getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGBGAG1hPHWm5Or2Glr25z65gHXF9Wzvg",
-  authDomain: "fadhilnigo-porto.firebaseapp.com",
-  projectId: "fadhilnigo-porto",
-  storageBucket: "fadhilnigo-porto.appspot.com",
-  messagingSenderId: "772895862137",
-  appId: "1:675183936714:web:384d6b3be0e9ef91c56033"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
